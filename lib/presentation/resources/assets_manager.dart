c554986d7bdced07851a349177f69cbd8ac1c105
prabinay <prabiney.garcia.23@gmail.com>
2023-03-18 22:53:25 +0545

    added resources folder

---
+++ lib/presentation/resources/assets_manager.dart
@@ -0,0 +1,16 @@
+const String imagePath = "assets/images";
+const String iconPath = "assets/icons";
+
+class ImageAssets{
+
+  // Social media icons
+  static const String googleIcon = "$iconPath/google.svg";
+  static const String twitterIcon = "$iconPath/twitter.svg";
+  static const String facebookIcon = "$iconPath/facebook.svg";
+  static const String loginIcon = "$iconPath/login_user.svg";
+  static const String likeIcon = "$iconPath/like.png";
+  static const String logoIcon = "$iconPath/logo.png";
+
+  static const String profilePic = "$imagePath/deadpool.png";
+
+}
